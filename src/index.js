213aeb0a6ac1610842a7698c6201d305b5eabf5c
nmclark007 <nicholemclark@yahoo.com>
2022-06-01 21:41:36 -0600

    setup format for daily weather API call

--- src/index.js
+++ src/index.js
@@ -28,6 +28,28 @@ let h1 = document.querySelector("#time");
 h1.innerHTML = formatDate(currentDate);
 //
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+<div class="weekly-forecast" id="forecast">
+               <div class="forecast-day">${day}</div>
+              <img id="forecasticon" src="" />
+             <div class="high">75° <span class="low">34°</span></div>
+            </div>
+          </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function updateSearch(event) {
   event.preventDefault();
   let weatherUpdate = document.querySelector("#city");
@@ -56,14 +78,20 @@ function getWeatherApi(city) {
 }
 
 //Bonus Week 5
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let apiCode = "https://api.openweathermap.org/data/2.5/weather";
-  let apiElement = `${apiCode}?lat=${lat}&lon=${long}&units=imperial`;
-  axios
-    .get(`${apiElement}&appid=0c9ee1c46913511e8bf8aca53ad61bfd`)
-    .then(showWeather);
+//function showPosition(position) {
+//let lat = position.coords.latitude;
+//let long = position.coords.longitude;
+// let apiCode = "https://api.openweathermap.org/data/2.5/weather";
+// let apiElement = `${apiCode}?lat=${lat}&lon=${long}&units=imperial`;
+// axios
+//  .get(`${apiElement}&appid=0c9ee1c46913511e8bf8aca53ad61bfd`)
+//   .then(showWeather);
+//}
+
+function getForecast(coordinates) {
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=0c9ee1c46913511e8bf8aca53ad61bfd&units=imperial`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
 }
 
 function showWeather(response) {
@@ -95,6 +123,8 @@ function showWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function showCelsiusTemp(event) {
